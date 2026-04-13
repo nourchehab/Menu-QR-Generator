4e0776e6dc3d3414b653c2e5730c63ac08ec826c
Jason Keyrouz <jtk06@mail.aub.edu>
2026-04-13 20:07:06 +0300

    Fixed existing restaurant case

--- src/test/java/com/restaurant/admin/controller/RestaurantControllerTest.java
+++ src/test/java/com/restaurant/admin/controller/RestaurantControllerTest.java
@@ -10,7 +10,7 @@ import com.restaurant.admin.service.SimpleUserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtendWith;      
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
