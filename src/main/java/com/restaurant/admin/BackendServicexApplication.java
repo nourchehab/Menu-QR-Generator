bb5677c56234a73faadde2cd16747da96ce17fc3
Mohd Katergi <mwk21@mail.aub.edu>
2026-04-26 23:58:13 +0300

    Caching enabled

--- src/main/java/com/restaurant/admin/BackendServicexApplication.java
+++ src/main/java/com/restaurant/admin/BackendServicexApplication.java
@@ -2,7 +2,10 @@ package com.restaurant.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,5 +13,10 @@ public class BackendServicexApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendServicexApplication.class, args);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("restaurantItems", "branchItems");
     }
 }
