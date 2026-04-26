31332256a6e3bff68fea388e8396d1364b401151
Jason Keyrouz <jtk06@mail.aub.edu>
2026-04-27 01:07:12 +0300

    Done with load testing with k6 and performance optimization (caching + DB tuning)

--- k6-tests/public-read-load.js
+++ k6-tests/public-read-load.js
@@ -1,34 +1,40 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 const BASE_URL = 'http://localhost:8081';
 
-// CHANGE THESE
-const RESTAURANT_ID = 25;
-const BRANCH_ID = 25;
+const RESTAURANT_ID = 4;
+const BRANCH_ID = 138;
 
 export const options = {
-    stages: [
-        { duration: '30s', target: 20 },
-        { duration: '30s', target: 40 },
-        { duration: '30s', target: 60 },
-        { duration: '1m', target: 60 },  // hold at 60
-        { duration: '30s', target: 0 },
-    ],
-    thresholds: {
-        http_req_failed: ['rate<0.05'],
-        http_req_duration: ['p(95)<3000'], // slightly relaxed
-    },
+  stages: [
+    { duration: '30s', target: 50 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 150 },
+    { duration: '30s', target: 200 },
+    { duration: '1m', target: 200 },
+    { duration: '30s', target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.05'],
+    http_req_duration: ['p(95)<1000'],
+  },
 };
 
 export default function () {
     const res1 = http.get(`${BASE_URL}/api/public/restaurants/${RESTAURANT_ID}/items`);
+
     check(res1, {
-        'restaurant items 200': (r) => r.status === 200,
+        'restaurant items 200': function (r) {
+            return r.status === 200;
+        },
     });
 
     const res2 = http.get(`${BASE_URL}/api/public/branch/${BRANCH_ID}/items`);
+
     check(res2, {
-        'branch items 200': (r) => r.status === 200,
+        'branch items 200': function (r) {
+            return r.status === 200;
+        },
     });
 }
